841397d269b17029e377a11b5b50d963014d37f3
Mumin Suleiman <9018970@student.zadkine.nl>
2025-04-14 18:25:00 +0200

    .

--- scripts/main.js
+++ scripts/main.js
@@ -65,32 +65,18 @@ class Game {
 
         this.autoClickers.forEach(clicker => {
             clicker.amount = 0;
-<<<<<<< HEAD
-            clicker.currentPrice = clicker.basePrice;  // Reset price
-=======
             clicker.currentPrice = clicker.basePrice;
->>>>>>> origin/main
             clicker.save();
         });
     
 
         this.upgrades.forEach((upgrade, index) => {
-<<<<<<< HEAD
-            upgrade.purchased = false;  
-            const button = document.getElementById(`upgrade${index}`);
-            if (button) {
-                button.disabled = false;  
-                button.innerText = `${upgrade.name} (⚡ ${upgrade.basePrice})`;  // Reset button text
-            }
-            // Clear upgrade purchase
-=======
             upgrade.purchased = false;
             const button = document.getElementById(`upgrade${index}`);
             if (button) {
                 button.disabled = false; // Enable the button
                 button.innerText = `${upgrade.name} (⚡ ${upgrade.basePrice})`;
             }
->>>>>>> origin/main
             localStorage.removeItem(`upgrade_${upgrade.name}`);
         });
     
